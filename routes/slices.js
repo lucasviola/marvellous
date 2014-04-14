8083246afe11012adf668a8bd3f7712d8f7ed61e
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-04-14 13:57:06 -0300

    created db open function

--- routes/slices.js
+++ routes/slices.js
@@ -7,6 +7,20 @@ var BSON = mongo.BSONPure;
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = new Db('slicesdb', server);
 
+db.open(function(err, db){
+	if(!err){
+		console.log("Connected to 'slicesdb' database");
+		db.collection('slices', {strict: true}, function(err, collection){
+			if(err){
+				console.log("The 'slices' connection doesn't exist. Creating it with sample data...");
+				populateDB();
+			}	
+		});	
+		
+	}	
+});
+
+
 exports.findAll = function(req, res){
 	res.send([{name:'website1'}, {name:'website2'}, {name:'website3'}]);
 };
