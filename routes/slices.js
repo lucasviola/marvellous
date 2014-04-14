10c63d0097b6aa72db0ef3f3161f018b632b0b89
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-04-14 13:48:42 -0300

    configured mongodb connection

--- routes/slices.js
+++ routes/slices.js
@@ -1,3 +1,12 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('slicesdb', server);
+
 exports.findAll = function(req, res){
 	res.send([{name:'website1'}, {name:'website2'}, {name:'website3'}]);
 };
